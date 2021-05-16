a0fbab8f46cb3a553c76f31ed9a805bac05ef52c
Jadhav <mahesh.jadhav@capgemini.com>
2021-05-16 18:01:22 +0530

    test tfplan

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -1,5 +1,11 @@
 #-- to maintain state
 terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "=2.46.0"
+    }
+  }
   backend "azurerm" {
     resource_group_name  = "terraform-state-test-rg"
     storage_account_name = "terraformstatetest2"
--- samples/integration-testing/src/provider.tf
+++ samples/integration-testing/src/provider.tf
@@ -1,4 +1,5 @@
 provider "azurerm" {
   skip_provider_registration = true
   features {}
+  subscription_id = "bdb791b7-d558-49fb-8529-ac5306c76b2d"
 }
