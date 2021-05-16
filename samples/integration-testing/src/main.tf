cb46c2a71e538309237b6e91a492a1430b9cc6dd
Jadhav <mahesh.jadhav@capgemini.com>
2021-05-16 17:44:33 +0530

    test tfplan

--- samples/integration-testing/src/main.tf
+++ samples/integration-testing/src/main.tf
@@ -1,10 +1,10 @@
 #-- to maintain state
 terraform {
   backend "azurerm" {
-    resource_group_name  = "terraform-state-test-rg"
-    storage_account_name = "terraformstatetest2"
-    container_name       = "tfstate"
-    key                  = "terraform.state"
+    #resource_group_name  = "terraform-state-test-rg"
+    #storage_account_name = "terraformstatetest2"
+    #container_name       = "tfstate"
+    #key                  = "terraform.state"
   }
   required_version = ">= 0.13"
 }
