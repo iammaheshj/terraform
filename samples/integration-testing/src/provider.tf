bdde81f42478b54f5bd0d63d937358904822b9e5
Jadhav <mahesh.jadhav@capgemini.com>
2021-05-16 19:53:36 +0530

    test tfplan

--- samples/integration-testing/src/provider.tf
+++ samples/integration-testing/src/provider.tf
@@ -1,5 +1,5 @@
 provider "azurerm" {
   skip_provider_registration = true
   features {}
-  subscription_id = "bdb791b7-d558-49fb-8529-ac5306c76b2d"
+  subscription_id = "8a9eee85-02f0-42d4-aabb-57d185d70706"
 }
